f1030eeacb10c93456105cb73258194a03776723
Allen Ford <allen@cyfordtechnologies.com>
2025-06-25 09:04:47 -0400

    Update configuration file

--- config.php
+++ config.php
@@ -6,11 +6,19 @@ return [
         'login_endpoint' => 'https://api.cyfordtechnologies.com/api/auth/v1/login', // Login URL
         'report_endpoint' => 'https://api.cyfordtechnologies.com/api/security/v1/report-ip', // Report URL
     ],
+    'postfix' => [
+        'main_config' => '/etc/postfix/main.cf', // Path to Postfix main configuration file
+        'master_config' => '/etc/postfix/master.cf', // Path to Postfix master configuration file
+        'postfix_command' => '/usr/sbin/postfix', // Path to Postfix executable
+        'allow_modification' => true, // Set to true to allow automatic configuration changes
+        'backup_directory' => '/var/backups/postfix', // Path to store configuration file backups
+    ],
     'credentials' => [
         'email' => '',  // Account email for login
         'password' => '',             // Account password for login
     ],
     'errors' => [
         'report_errors' => 1,
+        'error_log_location' =>  __DIR__ . '/errors/error.log',
     ],
 ];
